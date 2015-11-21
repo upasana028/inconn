0280acde8de01d1a73f61d2762450db861f719cd
upasana028 <028upasana820@gmail.com>
2015-11-22 01:53:14 +0530

    driver class for test

--- driver.php
+++ driver.php
@@ -6,4 +6,5 @@ $hostname = "localhost";
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) 
   or die("Unable to connect to MySQL");
-echo "Connected to MySQL<br>"
+echo "Connected to MySQL<br>";
+

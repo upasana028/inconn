79db6841846787fdf0bb8aa8068784fcd436c9a2
upasana028 <028upasana820@gmail.com>
2015-11-22 01:50:26 +0530

    driver class for test

---
+++ driver.php
@@ -0,0 +1,9 @@
+<?php
+$username = "learnmyw_root";
+$password = "Clock@1234";
+$hostname = "localhost"; 
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+  or die("Unable to connect to MySQL");
+echo "Connected to MySQL<br>"

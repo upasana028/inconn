6553938ac8cbd966567dadfcf2a3dfe8f79032c6
upasana028 <028upasana820@gmail.com>
2015-11-21 17:11:23 +0530

    DATA in conn

--- connDO.php
+++ connDO.php
@@ -1,1 +1,12 @@
 <?php
+
+public class connDO{
+private $username;
+private $password;
+private $domain;
+public function connDO($usr,$pass,$dom){
+	$username = $usr;
+	$password = $pass;
+	$domain = $dom;
+	}
+}

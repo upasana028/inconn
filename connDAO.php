51c926ae9e9ba21e099695b3cd0ba0f7ab8b5689
upasana028 <028upasana820@gmail.com>
2015-11-22 12:09:06 +0530

    connectioon

--- connDAO.php
+++ connDAO.php
@@ -9,12 +9,8 @@ function connDAO($connDo){
 
 function testMe(){
 	echo "Inside DAO";
-	$this->connDO->testMe();
 	}
 }
 
-$username = "learnmyw_root";
-$password = "Clock@1234";
-$hostname = "localhost"; 
-$conndao = new connDAO(new connDO($hostname,$username,$password));
+$conndao = new connDAO(connDO::getInstance());
 $conndao->testMe();
--- connDO.php
+++ connDO.php
@@ -1,16 +1,22 @@
 <?php
 
 class connDO{
+static $connObject = NULL;
 private $username;
 private $password;
 private $domain;
-function connDO($usr,$pass,$dom){
-	$this->username = $usr;
-	$this->password = $pass;
-	$this->domain = $dom;
+private function connDO(){
+	$this->username = "learnmyw_root";
+	$this->password = "Clock@1234";
+	$this->domain = "localhost";
 	}
 
-function testMe(){
-	echo $this->username ;
+static function getInstance(){
+	echo "DO called";
+	if($this->connObject == NULL)
+	{
+	$this->connObject =new connDO();	
+	}
+	return $this->connObject ;
 	}
 }
